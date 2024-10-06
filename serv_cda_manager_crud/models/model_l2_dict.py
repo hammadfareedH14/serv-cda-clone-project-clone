cd9c58a0929c04a5a4743c4961f1135e8ab81137
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-10-05 22:40:20 -0700

    model_l2_dict.py

--- serv_cda_manager_crud/models/model_l2_dict.py
+++ serv_cda_manager_crud/models/model_l2_dict.py
@@ -1,8 +1,9 @@
-from . import connection, uuid, ObjectId
+from db_con import connection
+from bson import ObjectId
 
 # Connect to MongoDB
 db = connection.db_client_atlas()
-l2_dict_collection = db["L2_dict"]
+L2_dict_collection = db["L2_dict"]
 
 class L2DictModel:
     def __init__(self, id_lv2, item_lv2_name_en, lv2_condition, undefined, abnormal, normal, borderline):
@@ -16,31 +17,38 @@ class L2DictModel:
 
     def save_to_db(self):
         l2_dict_item = {
-            'Id_lv2': uuid.uuid4().hex,
-            'item_lv2_NameEn': self.item_lv2_name_en,
+            'id_lv2': self.id_lv2,
+            'item_lv2_name_en': self.item_lv2_name_en,
             'lv2_condition': self.lv2_condition,
             'undefined': self.undefined,
             'abnormal': self.abnormal,
             'normal': self.normal,
             'borderline': self.borderline
         }
-        return l2_dict_collection.insert_one(l2_dict_item)
+        return L2_dict_collection.insert_one(l2_dict_item)
 
-    @staticmethod
-    def get_all():
-        return list(l2_dict_collection.find())
-
-    @staticmethod
-    def get_by_id(item_id):
-        return l2_dict_collection.find_one({'_id': ObjectId(item_id)})
 
     @staticmethod
     def update_by_id(item_id, data):
-        return l2_dict_collection.update_one(
+        return L2_dict_collection.update_one(
             {'_id': ObjectId(item_id)},
             {'$set': data}
         )
 
     @staticmethod
     def delete_by_id(item_id):
-        return l2_dict_collection.delete_one({'_id': ObjectId(item_id)})
+        return L2_dict_collection.delete_one({'_id': ObjectId(item_id)})
+
+    @staticmethod
+    def get_by_id(_id):
+        result = L2_dict_collection.find_one({'_id': ObjectId(_id)})
+        if result:
+            result['_id'] = str(result['_id'])  
+        return result
+    
+    @staticmethod
+    def get_all():
+        results = list(L2_dict_collection.find())
+        for result in results:
+            result['_id'] = str(result['_id'])  
+        return results
