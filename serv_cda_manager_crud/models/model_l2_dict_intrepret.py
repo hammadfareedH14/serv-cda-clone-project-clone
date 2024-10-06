a940c727913f1c36113fb4775cf3a8b99e7ead71
hammadfareedH14 <166123272+hammadfareedH14@users.noreply.github.com>
2024-10-05 20:16:52 -0700

    model_lintrepret.py

--- serv_cda_manager_crud/models/model_l2_dict_intrepret.py
+++ serv_cda_manager_crud/models/model_l2_dict_intrepret.py
@@ -1,11 +1,15 @@
-from . import connection, uuid, ObjectId
+
+from db_con import connection
+from bson import ObjectId
 
 # Connect to MongoDB
 db = connection.db_client_atlas()
-l2_dict_interpret_collection = db["L2_dict_interpret"]
+interpret_collection = db["L2_dict_interpret"]
 
-class L2DictInterpretModel:
-    def __init__(self, id_lv2, item_lv2_name_en, lv2_condition, notexamined, abnormal, normal, borderline, grade1, grade2, grade3, grade4, grade5, grade6):
+class InterpretModel:
+    def __init__(self, id_lv2, item_lv2_name_en, lv2_condition, notexamined, abnormal, normal, borderline,
+                grade1, grade2, grade3, grade4, grade5, grade6):
+        
         self.id_lv2 = id_lv2
         self.item_lv2_name_en = item_lv2_name_en
         self.lv2_condition = lv2_condition
@@ -22,7 +26,7 @@ class L2DictInterpretModel:
 
     def save_to_db(self):
         l2_dict_interpret_item = {
-            'Id_lv2': uuid.uuid4().hex,
+            'Id_lv2': self.Id_lv2,
             'item_lv2_NameEn': self.item_lv2_name_en,
             'lv2_condition': self.lv2_condition,
             'notexamined': self.notexamined,
@@ -36,23 +40,30 @@ class L2DictInterpretModel:
             'grade5': self.grade5,
             'grade6': self.grade6
         }
-        return l2_dict_interpret_collection.insert_one(l2_dict_interpret_item)
+        return interpret_collection.insert_one(l2_dict_interpret_item)
 
-    @staticmethod
-    def get_all():
-        return list(l2_dict_interpret_collection.find())
-
-    @staticmethod
-    def get_by_id(item_id):
-        return l2_dict_interpret_collection.find_one({'_id': ObjectId(item_id)})
 
     @staticmethod
     def update_by_id(item_id, data):
-        return l2_dict_interpret_collection.update_one(
+        return interpret_collection.update_one(
             {'_id': ObjectId(item_id)},
             {'$set': data}
         )
 
     @staticmethod
     def delete_by_id(item_id):
-        return l2_dict_interpret_collection.delete_one({'_id': ObjectId(item_id)})
+        return interpret_collection.delete_one({'_id': ObjectId(item_id)})
+
+    @staticmethod
+    def get_by_id(_id):
+        result = interpret_collection.find_one({'_id': ObjectId(_id)})
+        if result:
+            result['_id'] = str(result['_id'])  
+        return result
+    
+    @staticmethod
+    def get_all():
+        results = list(interpret_collection.find())
+        for result in results:
+            result['_id'] = str(result['_id'])  
+        return results
